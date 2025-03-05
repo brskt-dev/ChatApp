e78832dc84ee3ff2e287af8fc9b7ec6f9f88615e
eduardoarppen <eduardo.arine@arppen.com>
2025-03-05 14:43:27 -0300

    feat: teste migrations

---
+++ chat-api/Migrations/20250305174124_UpdateTeste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chat_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTeste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
